0d5fc22a49aaeab613f45aa5fcbb8bcfaf286bf4
Alexander <90058418+David-Alezander-Jiron@users.noreply.github.com>
2024-07-10 05:19:33 -0500

    Add files via upload

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="my-component">
+        <h1 v-if="isVisible">{{ message }}</h1>
+        <button v-on:click="isVisible = !isVisible">Cambiar visibilidad</button>
+        <input v-model="message">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data(){
+        return {
+            message: 'Que onda gente',
+            isVisible: true
+        }
+    },
+    methods: {
+        toggleVisibility(){
+            this.isVisible = !this.isVisible;
+        }
+    }
+}
+</script>
